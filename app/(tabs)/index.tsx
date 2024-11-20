33ea0c1c1985c524298f55a59caf797ca6e27a0a
André Pereira <andre.jobs23@gmail.com>
2024-11-20 19:17:58 -0300

    Lamp svg

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Text, View, StyleSheet, TextInput} from 'react-native';
-
-
+import Lamp from '@/assets/images/lamp.tsx';
 
 const Index = () => {
   const [text, onChangeText] = React.useState('Gansos...');
 
   return (
     <View style={styles.container}>
+      <Lamp width={40} height={40} style={styles.marginBottom} />
       <Text>Me sugira uma ideia sobre...</Text>
       <TextInput style={styles.input} value={text} onChangeText={onChangeText}/>
     </View>
@@ -15,6 +15,10 @@ const Index = () => {
 };
 
 const styles = StyleSheet.create({
+  marginBottom: {
+    marginBottom: 8,
+  },
+
   container: {
     backgroundColor: '#c9c9c9',
     flex: 1,
